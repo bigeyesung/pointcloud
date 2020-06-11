215fea2dafe853e877fb582e8ae911db2d1548fa
chenhsi <sungchenhsi@gmail.com>
2020-06-11 21:15:00 +0100

    add functions to check different scanners

--- pointmerge.py
+++ pointmerge.py
@@ -1,5 +1,7 @@
 import open3d as o3d
 import numpy as np
+import os
+import pye57
 pcd1 = o3d.io.read_point_cloud("p1.pcd")
 pcd2 = o3d.io.read_point_cloud("p2.pcd")
 def mergefunc(pcd1,pcd2):
@@ -19,3 +21,34 @@ def mergefunc(pcd1,pcd2):
         o3d.visualization.draw_geometries([pcd])
     else:
         print("pcd2 is empty")
+
+def ShowScanLoc(file):
+        
+    pcd = o3d.geometry.PointCloud()
+    count = 0
+    for e57File in os.listdir(file):
+        if e57File.endswith(".e57"):
+            with pye57.E57(file + "/" + e57File) as e57:
+                
+                for idx in range(e57.scan_count):
+                    data = e57.read_scan(idx, ignore_missing_fields = True)
+                    numPoints = data["cartesianX"].shape[0] # instead of header.point_count as the latter is often too large
+                    xyz = np.zeros((numPoints, 3))
+                    xyz[:, 0] = np.reshape(data["cartesianX"], -1)
+                    xyz[:, 1] = np.reshape(data["cartesianY"], -1)
+                    xyz[:, 2] = np.reshape(data["cartesianZ"], -1)
+                    tmp = o3d.geometry.PointCloud()
+                    tmp.points = o3d.utility.Vector3dVector(xyz)
+                    if count == 0:
+                        tmp.paint_uniform_color([1, 0.706, 0])
+                    else:
+                        tmp.paint_uniform_color([0, 0.5, 0.5])
+                    o3d.visualization.draw_geometries([tmp])
+                    pcd += tmp.voxel_down_sample(0.02)
+                    o3d.visualization.draw_geometries([pcd])
+                count += 1
+    o3d.visualization.draw_geometries([pcd])
+
+
+if __name__ == "__main__":
+    ShowScanLoc("input/")
